a4f9f9177a756add52a0f13687325803d77aebd8
Erk <61740386+pntErk@users.noreply.github.com>
2023-05-27 02:01:34 +0700

    components: create 'InputField.tsx'

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,10 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import TodoList from './components/TodoList'
+import InputField from './components/InputField'
 
-function App() {
+const App: React.FC = () =>{
   const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
@@ -29,7 +29,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-      <TodoList />
+      <InputField />
     </>
   )
 }
---
+++ src/components/InputField.tsx
@@ -0,0 +1,9 @@
+const InputField = () => {
+  return (
+    <form className="input">
+      <input type="input" placeholder="Enter a task" className="inputBox" />
+    </form>
+  );
+};
+
+export default InputField;
