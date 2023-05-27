b768c9ac8c4b838eece4ba369dbd09dc15d7f0aa
Erk <61740386+pntErk@users.noreply.github.com>
2023-05-27 15:28:13 +0700

    split task active&complete
    for implement draggable method

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -9,15 +9,33 @@ interface Props {
 }
 const TodoList: React.FC<Props> = ({ todos, setTodos }: Props) => {
   return (
-    <div className="todos">
-      {todos.map((todo) => (
-        <SingleTodo
-          todo={todo}
-          key={todo.id}
-          todos={todos}
-          setTodos={setTodos}
-        />
-      ))}
+    // <div className="todos">
+    //   {todos.map((todo) => (
+    //     <SingleTodo
+    //       todo={todo}
+    //       key={todo.id}
+    //       todos={todos}
+    //       setTodos={setTodos}
+    //     />
+    //   ))}
+    // </div>
+    <div className="container">
+      <div className="todos">
+        <span className="todos__heading">
+          Active Tasks
+        </span>
+        {
+          todos.map((todo) => (<SingleTodo todo={todo} todos={todos} key={todo.id} setTodos={setTodos}/>))
+        }
+      </div>
+      <div className="todos remove">
+      <span className="todos__heading">
+          Completed Tasks
+        </span>
+        {
+          todos.map((todo) => (<SingleTodo todo={todo} todos={todos} key={todo.id} setTodos={setTodos}/>))
+        }
+      </div>
     </div>
   );
 };
