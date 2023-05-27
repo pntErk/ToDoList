fce687b7d59a12230f36f35f1c059014aa100efa
Erk <61740386+pntErk@users.noreply.github.com>
2023-05-27 11:41:41 +0700

    feat: create input ui

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,7 +1,11 @@
+import "./styles.css";
 const InputField = () => {
   return (
     <form className="input">
-      <input type="input" placeholder="Enter a task" className="inputBox" />
+      <input type="input" placeholder="Enter a task" className="input__box" />
+      <button className="input_submit" type="submit">
+        Add
+      </button>
     </form>
   );
 };
